cc56b033c87d993d8d17a56af5d65dff6979abdf
root <ppp.shiro46@gmail.com>
2021-02-16 17:54:14 +0000

    init jest

---
+++ test/js/Hello.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import Hello from '../../Hello';
+import Hello from '../../frontend/components/hello'
+import renderer from 'react-test-renderer';
+
+test('Hello', () => {
+    const component = renderer.create(
+      <Hello />
+    )
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
